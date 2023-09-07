157fcb374f01b97653c2907b1fc3072bde507cad
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-09-07 10:25:36 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,48 @@
 #!/usr/bin/python3
 """
 Script that distributes an archive to your web servers,
-using the function do_deploy.
+using the function do_deploy
 """
 from fabric.api import *
-from os.path import exists
 from datetime import datetime
+from os.path import exists
 
 env.hosts = ["18.204.14.252", "107.22.144.202"]
-env.user = "ubuntu"
 
+
+def do_pack():
+    """
+    Creates .tgz archive from contents of web_static folder.
+    """
+    try:
+        local("mkdir -p versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as e:
+        return None
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to teh web servers.
+    Distributes an archive to my web servers.
     """
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
     try:
+        if do_pack() is None:
+            return False
         put(archive_path, "/tmp/")
-        folder_name = archive_path.split("/")[-1].split(".")[0]
-        release_path = "/data/web_static/releases/{}".format(folder_name)
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf /tmp/{}.tgz -C {}".format(folder_name, release_path))
-        run("rm /tmp/{}.tgz".format(folder_name))
-        run("mv {}/web_static/* {}".format(release_path, release_path))
-        run("rm -rf {}/web_static".format(release_path))
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_path))
-        print("New version deployed!")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
         return True
     except Exception as error:
         return False
