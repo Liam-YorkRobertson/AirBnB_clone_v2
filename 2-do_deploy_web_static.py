2870ca7d7608278bed2654df52e67af688d9f944
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-09-07 18:21:43 -0700

    test :,)

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ Script that distributes an archive to your web servers,
 using the function do_deploy.
 """
 from fabric.api import *
-from os.path import exists
+from os.path import exists, join
 from datetime import datetime
 
 env.hosts = ["18.204.14.252", "107.22.144.202"]
@@ -12,17 +12,23 @@ env.hosts = ["18.204.14.252", "107.22.144.202"]
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to teh web servers.
+    Distributes an archive to the web servers.
     """
     if not exists(archive_path):
         return False
+
+    filename = archive_path.split('/')[-1]
+    folder_name = filename.split('.')[0]
+    no_tgz = join("/data/web_static/releases", folder_name)
+    tmp = join("/tmp", filename)
+
     try:
         put(archive_path, "/tmp/")
-        folder_name = archive_path.split("/")[-1].split(".")[0]
-        release_path = "/data/web_static/releases/{}".format(folder_name)
+        release_path = join("/data/web_static/releases", folder_name)
+
         run("mkdir -p {}".format(release_path))
-        run("tar -xzf /tmp/{}.tgz -C {}".format(folder_name, release_path))
-        run("rm /tmp/{}.tgz".format(folder_name))
+        run("tar -xzf {} -C {}".format(tmp, release_path))
+        run("rm {}".format(tmp))
         run("mv {}/web_static/* {}".format(release_path, release_path))
         run("rm -rf {}/web_static".format(release_path))
         run("rm -rf /data/web_static/current")
