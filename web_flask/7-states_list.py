4b7843c814040d7329aad9755663981c35574a00
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-09-24 16:04:02 -0700

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,15 +3,14 @@
 Script that starts a flask web application.
 """
 from flask import Flask, render_template
-from models import *
 from models import storage
-from models.state import state
+from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.route("/states_list, strict_slashes=False")
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
