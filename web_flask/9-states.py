718415385787d67e3eb26a3536c94410241b30cf
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-09-24 18:32:28 -0700

    test

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""
-Script that starts a flask web application.
-"""
-from flask import Flask, render_template
+""" script that starts a Flask web application """
 from models import storage
-from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State).values()
-    return render_template("9-states.html", states=states)
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
-def state_cities(id):
-    state = storage.get(State, id)
-    if state:
-        return render_template("9-states.html", state=state)
-    return render_template("9-states.html", not_found=True)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
